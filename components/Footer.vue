4459e54122761bf69d9cc86558c8daefb614d07a
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-02-06 15:48:18 -0300

    Feature: Privacy Policy

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="bg-zinc-900 h-16 w-full mt-24 flex items-center justify-center">
+  <div
+    class="bg-zinc-900 h-16 w-full mt-24 flex flex-col items-center justify-center"
+  >
     <ClientOnly>
       <p class="text-xs text-zinc-400">
         © {{ currentYear }} Copyright -
         <span class="font-bold">ShinodaLabs</span>
       </p>
+      <div class="flex flex-row gap-2">
+        <p
+          @click="goToPolicyPrivacy"
+          class="text-xs text-zinc-400 cursor-pointer"
+        >
+          Política de Privacidade
+        </p>
+      </div>
     </ClientOnly>
   </div>
 </template>
@@ -12,9 +22,15 @@
 <script setup>
 import moment from "moment";
 
+const router = useRouter();
+
 const currentYear = computed(() => {
   return moment().year();
 });
+
+const goToPolicyPrivacy = () => {
+  router.push("/policy-privacy");
+};
 </script>
 
 <style></style>
---
+++ pages/policy-privacy.vue
@@ -0,0 +1,189 @@
+<template>
+  <div id="Search" class="w-full h-full flex justify-center">
+    <div
+      class="max-w-container w-full pt-12 px-2 flex flex-col gap-10 border-t border-t-zinc-600"
+    >
+      <h2>
+        <span class="text-white text-2xl font-bold">Política Privacidade</span>
+      </h2>
+      <p>
+        <span class="text-white"
+          >A sua privacidade é importante para nós. É política do FSW Barber
+          respeitar a sua privacidade em relação a qualquer informação sua que
+          possamos coletar no site
+          <a href="https://fsw-barber.vercel.app/">FSW Barber</a>, e outros
+          sites que possuímos e operamos.</span
+        >
+      </p>
+      <p>
+        <span class="text-white"
+          >Solicitamos informações pessoais apenas quando realmente precisamos
+          delas para lhe fornecer um serviço. Fazemo-lo por meios justos e
+          legais, com o seu conhecimento e consentimento. Também informamos por
+          que estamos coletando e como será usado.</span
+        >
+      </p>
+      <p>
+        <span class="text-white"
+          >Apenas retemos as informações coletadas pelo tempo necessário para
+          fornecer o serviço solicitado. Quando armazenamos dados, protegemos
+          dentro de meios comercialmente aceitáveis ​​para evitar perdas e
+          roubos, bem como acesso, divulgação, cópia, uso ou modificação não
+          autorizados.</span
+        >
+      </p>
+      <p>
+        <span class="text-white"
+          >Não compartilhamos informações de identificação pessoal publicamente
+          ou com terceiros, exceto quando exigido por lei.</span
+        >
+      </p>
+      <p>
+        <span class="text-white"
+          >O nosso site pode ter links para sites externos que não são operados
+          por nós. Esteja ciente de que não temos controle sobre o conteúdo e
+          práticas desses sites e não podemos aceitar responsabilidade por suas
+          respectivas&nbsp;</span
+        ><a
+          href="https://politicaprivacidade.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+          style="background-color: transparent"
+          class="text-white"
+          >políticas de privacidade</a
+        ><span class="text-white">.</span>
+      </p>
+      <p>
+        <span class="text-white"
+          >Você é livre para recusar a nossa solicitação de informações
+          pessoais, entendendo que talvez não possamos fornecer alguns dos
+          serviços desejados.</span
+        >
+      </p>
+      <p>
+        <span class="text-white"
+          >O uso continuado de nosso site será considerado como aceitação de
+          nossas práticas em torno de privacidade e informações pessoais. Se
+          você tiver alguma dúvida sobre como lidamos com dados do usuário e
+          informações pessoais, entre em contacto connosco.</span
+        >
+      </p>
+      <p>
+        <span class="text-white"
+          ><ul>
+            <li>
+              <span class="text-white"
+                >O serviço Google AdSense que usamos para veicular publicidade
+                usa um cookie DoubleClick para veicular anúncios mais relevantes
+                em toda a Web e limitar o número de vezes que um determinado
+                anúncio é exibido para você.</span
+              >
+            </li>
+            <li>
+              <span class="text-white"
+                >Para mais informações sobre o Google AdSense, consulte as FAQs
+                oficiais sobre privacidade do Google AdSense.</span
+              >
+            </li>
+            <li>
+              <span class="text-white"
+                >Utilizamos anúncios para compensar os custos de funcionamento
+                deste site e fornecer financiamento para futuros
+                desenvolvimentos. Os cookies de publicidade comportamental
+                usados ​​por este site foram projetados para garantir que você
+                forneça os anúncios mais relevantes sempre que possível,
+                rastreando anonimamente seus interesses e apresentando coisas
+                semelhantes que possam ser do seu interesse.</span
+              >
+            </li>
+            <li>
+              <span class="text-white"
+                >Vários parceiros anunciam em nosso nome e os cookies de
+                rastreamento de afiliados simplesmente nos permitem ver se
+                nossos clientes acessaram o site através de um dos sites de
+                nossos parceiros, para que possamos creditá-los adequadamente e,
+                quando aplicável, permitir que nossos parceiros afiliados
+                ofereçam qualquer promoção que pode fornecê-lo para fazer uma
+                compra.</span
+              >
+            </li>
+          </ul>
+          <p><br /></p
+        ></span>
+      </p>
+      <h3>
+        <span class="text-white text-2xl font-bold"
+          >Compromisso do Usuário</span
+        >
+      </h3>
+      <p>
+        <span class="text-white"
+          >O usuário se compromete a fazer uso adequado dos conteúdos e da
+          informação que o FSW Barber oferece no site e com caráter enunciativo,
+          mas não limitativo:</span
+        >
+      </p>
+      <ul>
+        <li>
+          <span class="text-white"
+            >A) Não se envolver em atividades que sejam ilegais ou contrárias à
+            boa fé a à ordem pública;</span
+          >
+        </li>
+        <li>
+          <span class="text-white"
+            >B) Não difundir propaganda ou conteúdo de natureza racista,
+            xenofóbica, </span
+          ><span class="text-white"
+            ><a
+              href="https://bbebbet.br.com"
+              style="
+                color: inherit !important;
+                text-decoration: none !important;
+                font-size: inherit !important;
+              "
+              >bbebbet</a
+            ></span
+          ><span class="text-white">
+            ou azar, qualquer tipo de pornografia ilegal, de apologia ao
+            terrorismo ou contra os direitos humanos;</span
+          >
+        </li>
+        <li>
+          <span class="text-white"
+            >C) Não causar danos aos sistemas físicos (hardwares) e lógicos
+            (softwares) do FSW Barber, de seus fornecedores ou terceiros, para
+            introduzir ou disseminar vírus informáticos ou quaisquer outros
+            sistemas de hardware ou software que sejam capazes de causar danos
+            anteriormente mencionados.</span
+          >
+        </li>
+      </ul>
+      <h3>
+        <span class="text-white text-2xl font-bold">Mais informações</span>
+      </h3>
+      <p>
+        <span class="text-white"
+          >Esperemos que esteja esclarecido e, como mencionado anteriormente, se
+          houver algo que você não tem certeza se precisa ou não, geralmente é
+          mais seguro deixar os cookies ativados, caso interaja com um dos
+          recursos que você usa em nosso site.</span
+        >
+      </p>
+      <p>
+        <span class="text-white"
+          >Esta política é efetiva a partir de&nbsp;6 February 2024 18:38</span
+        >
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  name: "PolictyPrivacy",
+  middleware: ["auth"],
+});
+</script>
+
+<style></style>
